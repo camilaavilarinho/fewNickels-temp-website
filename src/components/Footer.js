5b42dc68e1c8bc27070b7ae5a9367ed2cfea3958
Camila Vilarinho <camilaavilarinho@gmail.com>
2019-01-31 11:31:53 -0300

    Refactoring Footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,23 @@ import {
   faFacebookF,
   faTwitter,
   faInstagram,
-  /* faYoutube,
-  faGooglePlusG */
+  faTelegram
 } from "@fortawesome//free-brands-svg-icons";
+import { faMapMarkerAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import background from "../assets/images/platform1.png";
+
+const mediaQuery = "(max-width: 906px)";
 
 const Footer = styled.div`
   background-color: #ecf0f1;
-  background: linear-gradient(rgba(0, 50, 0, 0.5), rgba(0, 100, 0, 0.7)),
+  background: linear-gradient(rgba(42, 157, 114, 0.7), rgba(42, 157, 114, 0.7)),
     url(${background});
   background-size: 100%;
   background-position: top right;
   background-repeat: no-repeat;
   padding: 2rem;
   color: #dfe6e9;
-  text-align: center;
+  text-align: justify;
   font-weight: 300;
   min-height: 550px;
 `;
@@ -33,29 +35,32 @@ const Contact = styled.section`
 `;
 
 const About = styled.div`
-  & > a {
+  font-size: 18px;
+  & a {
     color: #dfe6e9;
     text-decoration: none;
   }
-  /* border-top: 2px solid #dfe6e9; */
-  /* border-left: 2px solid #ffffff; */
+  & span {
+    padding: 10px;
+  }
+`;
+
+const Content = styled.div`
+  margin-left: 10rem;
+  & p{
+    padding-top: 5px;
+  }
+  @media ${mediaQuery} {
+    margin-left: 0;
+  }
 `;
 
 const Social = styled.div``;
 
-/* const H3 = styled.h3`
-  font-size: 30px;
-`; */
-
 const H2 = styled.h2`
   margin: 1rem;
+  text-align: center;
 `;
-
-/* const H1 = styled.h1`
-  margin: 1rem 0 4rem 0;
-  font-size: 60px;
-  color: #ffffff;
-`; */
 
 const IconLink = styled.a`
   padding: 20px;
@@ -68,35 +73,53 @@ export default () => (
     <H1>Get in touch!</H1> */}
     <Contact>
       <About>
-        <H2 >About</H2>
-        <a href="mailto:fewnickels@fewnickels.com">fewnickels@fewnickels.com</a>
-        <p>Telegram: @TraderXCoin</p>
-        <p>Brasília, Federal District</p>
-        <p>Brazil</p>
+        <H2>About</H2>
+        <Content>
+          <a href="mailto:fewnickels@fewnickels.com">
+            <span>
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </span>
+            fewnickels@fewnickels.com
+          </a>
+          <p>
+            <span>
+              <FontAwesomeIcon icon={faTelegram} size="2x" />
+            </span>
+            @TraderXCoin
+          </p>
+          <p>
+            <span>
+              <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" />
+            </span>
+            Brasília, Federal District, Brazil
+          </p>
+        </Content>
       </About>
       <Social>
         <H2>Social</H2>
-        <IconLink
-          href="https://www.linkedin.com/company/fewnickels"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-        </IconLink>
-        <IconLink href="https://www.facebook.com/pg/FewNickels" target="_blank">
-          <FontAwesomeIcon icon={faFacebookF} size="2x" />
-        </IconLink>
-        <IconLink href="https://twitter.com/FewNickels" target="_blank">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
-        </IconLink>
-        <IconLink href="https://www.instagram.com/fewnickels/" target="_blank">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </IconLink>
-        {/* <IconLink href="" target="_blank">
-          <FontAwesomeIcon icon={faYoutube} size="2x" />
-        </IconLink>
-        <IconLink href="" target="_blank">
-          <FontAwesomeIcon icon={faGooglePlusG} size="2x" />
-        </IconLink> */}
+        <Content>
+          <IconLink
+            href="https://www.linkedin.com/company/fewnickels"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+          </IconLink>
+          <IconLink
+            href="https://www.facebook.com/pg/FewNickels"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faFacebookF} size="2x" />
+          </IconLink>
+          <IconLink href="https://twitter.com/FewNickels" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </IconLink>
+          <IconLink
+            href="https://www.instagram.com/fewnickels/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </IconLink>
+        </Content>
       </Social>
     </Contact>
   </Footer>
