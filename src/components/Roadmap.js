9880e6c38a1a8637f8bb0c15f1b5d964de187507
Camila Vilarinho <camilaavilarinho@gmail.com>
2019-02-01 11:22:08 -0300

    Updating Roadmap layout

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -9,6 +9,8 @@ import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
 
 const theme = createMuiTheme({
   palette: {
@@ -23,17 +25,31 @@ const theme = createMuiTheme({
 const styles = theme => ({
   root: {
     width: "100%",
-    backgroundColor: '#ffffff'
+    backgroundColor: "#ffffff"
   },
   header: {
     fontSize: "50px",
     color: "#726658",
     textAlign: "center",
-    paddingTop: '2rem'
+    paddingTop: "2rem"
+  },
+  phases: {
+    padding: "2rem"
+  },
+  phase: {
+    textAlign: "center",
+    color: "#726658",
+    fontWeight: "bold"
+  },
+  phaseCard: {
+    padding: 20
+  },
+  phaseHeader: {
+    color: "#2A9D72"
   },
   stepper: {
-    margin: "0 15rem",
-    paddingBottom: '2rem'
+    margin: "0 25rem",
+    paddingBottom: "2rem"
   },
   stepLabel: {
     fontSize: 20
@@ -50,7 +66,7 @@ const styles = theme => ({
     marginBottom: theme.spacing.unit * 2
   },
   resetContainer: {
-    margin: "2rem 15rem",
+    margin: "2rem 25rem",
     padding: theme.spacing.unit * 3
   }
 });
@@ -129,6 +145,37 @@ class VerticalLinearStepper extends React.Component {
       <MuiThemeProvider theme={theme}>
         <div className={classes.root} id="roadmap">
           <h1 className={classes.header}>Our Roadmap</h1>
+          <Grid container spacing={32} className={classes.phases}>
+            <Grid item xs className={classes.phase}>
+              <Card className={classes.phaseCard}>
+                <h3 className={classes.phaseHeader}>Phase 1</h3>
+                <p>
+                  Investment Platform - Rating System - Sell Contracts - Crops
+                  Monitor
+                </p>
+                {/* <p>Rating System</p>
+              <p>Sell Contracts</p>
+              <p>Crops Monitor</p> */}
+              </Card>
+            </Grid>
+            <Grid item xs className={classes.phase}>
+              <Card className={classes.phaseCard}>
+                <h3 className={classes.phaseHeader}>Phase 2</h3>
+                <p>
+                  Buy and sell products - Hedge Contracts - Dynamics Insurance
+                </p>
+                {/* <p>Hedge Contracts</p>
+              <p>Dynamics Insurance</p> */}
+              </Card>
+            </Grid>
+            <Grid item xs className={classes.phase}>
+              <Card className={classes.phaseCard}>
+                <h3 className={classes.phaseHeader}>Phase 3</h3>
+                <p>Crops ETF's</p>
+                <br />
+              </Card>
+            </Grid>
+          </Grid>
           <Stepper
             activeStep={activeStep}
             orientation="vertical"
@@ -186,9 +233,7 @@ class VerticalLinearStepper extends React.Component {
           </Stepper>
           {activeStep === steps.length && (
             <Paper square elevation={0} className={classes.resetContainer}>
-              <Typography>
-                All steps completed
-              </Typography>
+              <Typography>All steps completed</Typography>
               <Button
                 onClick={this.handleReset}
                 className={classes.button}
